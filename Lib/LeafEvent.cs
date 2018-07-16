5d3afbdce27bc55f9a1634596aa69829f1647291
zengjiyang <652105072@qq.com>
2018-07-16 12:43:05 +0800

    初始化

---
+++ Lib/LeafEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using LeafSoft.Model;
+/*---------------作者：叶知秋----------------------*/
+
+namespace LeafSoft.Lib
+{
+    public class LeafEvent
+    {
+        /// <summary>
+        /// 数据接收事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="data"></param>
+        public delegate void DataReceivedHandler(object sender,byte[] data);
+
+        /// <summary>
+        /// 发送数据事件
+        /// </summary>
+        /// <param name="data"></param>
+        public delegate bool DataSendHandler(byte[] data);
+    }
+}
